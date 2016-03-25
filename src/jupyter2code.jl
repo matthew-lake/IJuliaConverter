5ac1e5d5b82a66c9bde9fcbb3821e5f93ee53fb7
Matthew Lake <mgtlake@outlook.com>
2016-03-25 21:54:42 +1000

    Consider newline issues

--- src/jupyter2code.jl
+++ src/jupyter2code.jl
@@ -1,5 +1,7 @@
 using JSON
 
+lineSep = @windows ? "\r\n" : "\n"
+
 function jupyter2code(filenameIn::AbstractString, filenameOut::AbstractString; 
 		linewidth=80, tabwidth=4)
 	open(filenameIn, "r") do f
@@ -16,7 +18,8 @@ function jupyter2code(fileIn::IO, filenameOut::AbstractString; linewidth=80,
 
 	for cell in json["cells"]
 		if cell["cell_type"] == "code"
-			output *= join(cell["source"]) * "\n\n"
+			# TODO what if single string with different newlines to system
+			output *= join(cell["source"]) * lineSep^2
 		elseif cell["cell_type"] == "markdown"
 			# If list of strings, join together with no seperator as per docs
 			# (If not a list, then this will have no impact)
@@ -24,7 +27,7 @@ function jupyter2code(fileIn::IO, filenameOut::AbstractString; linewidth=80,
 			# Split the string into lines using '\n', then prepend a hash to 
 			# each line, then join and readd '\n'
 			# TODO address line seperator issue
-			output *= join(map(prepend_hash, split(content, '\n')), '\n') * "\n\n"
+			output *= join(map(prepend_hash, split(content, '\n')), '\n') * lineSep^2
 		end
 	end
 
